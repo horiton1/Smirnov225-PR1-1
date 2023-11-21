bf944c217a0b9ed36556594ebe6fd27c266a4684
Pavel Smirnov <pavelsmirnov954@gmail.com>
2023-11-21 09:52:20 +0300

    Добавил cs

--- Prak1/MainWindow.xaml.cs
+++ Prak1/MainWindow.xaml.cs
@@ -24,7 +24,15 @@ namespace Prak1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!double.TryParse(R1TextBox.Text, out double R1)) MessageBox.Show("Поле только для чисел");
+            if (!double.TryParse(R2TextBox.Text, out double R2)) MessageBox.Show("Поле только для чисел");
+            double p = 3.14;
+            double s1 = p * Math.Pow(R1, 2);
+            double s2 = p * Math.Pow(R2, 2);
+            double s3 = s1 - s2;
+            S1TextBox.Text += s1;
+            S2TextBox.Text += s2;
+            S3TextBox.Text += s3;
         }
     }
 }
